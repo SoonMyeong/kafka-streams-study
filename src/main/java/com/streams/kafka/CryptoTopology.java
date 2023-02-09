7803e36b81276242862ecfc16133845787579d01
SoonMyeong <soon_myeong@naver.com>
2023-02-10 01:24:05 +0900

    테스트 수정

--- src/main/java/com/streams/kafka/CryptoTopology.java
+++ src/main/java/com/streams/kafka/CryptoTopology.java
@@ -1,13 +1,16 @@
 package com.streams.kafka;
 
+import com.mitchseymour.kafka.serialization.avro.AvroSerdes;
+import com.streams.kafka.model.FilterRes;
 import com.streams.kafka.topic.Tweet;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.kstream.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class CryptoTopology {
@@ -16,14 +19,25 @@ public class CryptoTopology {
     void buildPipeline(StreamsBuilder streamsBuilder) {
         KStream<byte[], Tweet> stream =
                 streamsBuilder.stream("tweets", Consumed.with(Serdes.ByteArray(), Tweet.serdes()));
-        KStream<byte[], Tweet> filtered =
-                stream.filterNot(
-                        (key, tweet) -> tweet.isRetweet());
 
-        filtered.to(
-                "filter-res",
+        KStream<byte[], Tweet> filtered = stream.filterNot((key, tweet) -> tweet.isRetweet());
+
+        KStream<byte[], FilterRes> res = filtered.flatMapValues(
+                (tweet) -> {
+                    List<FilterRes> filterResList = new ArrayList<>();
+                    if("pass".equals(tweet.getText())) {
+                        FilterRes filterRes = new FilterRes();
+                        filterRes.setRetweet(false);
+                        filterRes.setText("success");
+                        filterResList.add(filterRes);
+                    }
+                    return filterResList;
+                }
+        );
+        res.to(
+                "result",
                 Produced.with(
                         Serdes.ByteArray(),
-                        com.mitchseymour.kafka.serialization.avro.AvroSerdes.get(FilterRes.class)));
+                       AvroSerdes.get(FilterRes.class)));
     }
 }
